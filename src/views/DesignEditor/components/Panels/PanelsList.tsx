6d11b68cfb005c6bd81e379c343fb4a87e11a03f
J7 <j7.dev.gg@outlook.com>
2023-05-30 02:03:13 +0800

    refactor: PanelsList

--- src/views/DesignEditor/components/Panels/PanelsList.tsx
+++ src/views/DesignEditor/components/Panels/PanelsList.tsx
@@ -1,10 +1,9 @@
 import { useStyletron, styled } from "baseui"
-import { BASE_ITEMS, VIDEO_PANEL_ITEMS } from "~/constants/app-options"
+import { BASE_ITEMS } from "~/constants/app-options"
 import useAppContext from "~/hooks/useAppContext"
 import Icons from "~/components/Icons"
 import { useTranslation } from "react-i18next"
 import useSetIsSidebarOpen from "~/hooks/useSetIsSidebarOpen"
-import useEditorType from "~/hooks/useEditorType"
 import Scrollable from "~/components/Scrollable"
 import { Block } from "baseui/block"
 
@@ -18,8 +17,7 @@ const Container = styled("div", (props) => ({
 const PanelsList = () => {
   const { activePanel } = useAppContext()
   const { t } = useTranslation("editor")
-  const editorType = useEditorType()
-  const PANEL_ITEMS = editorType === "VIDEO" ? VIDEO_PANEL_ITEMS : BASE_ITEMS
+  const PANEL_ITEMS = BASE_ITEMS
   return (
     <Container>
       <Scrollable autoHide={true}>
